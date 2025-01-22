b67a79d613f3e3a814b1afc36d2d19fcd9f47d56
HASSADZAKARIA <hassad.zakaria04@gmail.com>
2025-01-22 13:51:46 +0100

    site prefini

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,42 @@
 $(document).ready(function() {
-    var mixer = mixitup('.portfolio-container');
+    // Initialisation
+    let $grid = $('.portfolio-container');
+    let $items = $('.portfolio-item');
+    let $filters = $('#portfolio-flters li');
 
-    $('#portfolio-flters li').on('click', function() {
-        $('#portfolio-flters li').removeClass('filter-active');
+    // Fonction de filtrage
+    function filterItems(filter) {
+        $items.each(function() {
+            let $item = $(this);
+            if (filter === '*' || $item.hasClass(filter.substring(1))) {
+                $item.removeClass('hide').fadeIn(300);
+            } else {
+                $item.addClass('hide').fadeOut(300);
+            }
+        });
+    }
+
+    // Gestion des clics sur les filtres
+    $filters.on('click', function(e) {
+        e.preventDefault();
+        
+        // Mise à jour de la classe active
+        $filters.removeClass('filter-active');
         $(this).addClass('filter-active');
-        var filterValue = $(this).attr('data-filter');
-        mixer.filter(filterValue);
+
+        // Filtrage des éléments
+        let filterValue = $(this).attr('data-filter');
+        filterItems(filterValue);
     });
+
+    // Animation au défilement (si vous utilisez AOS)
+    if (typeof AOS !== 'undefined') {
+        AOS.init({
+            duration: 1000,
+            once: true
+        });
+    }
+
+    // Animation initiale
+    $items.addClass('animate').attr('data-animate', 'fadeInUp');
 });
-
-
